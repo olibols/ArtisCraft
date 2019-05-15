04576bd2becf6bf1c0a180626398795395a89df5
olibols <olibols@aol.com>
2019-05-15 16:29:21 +0100

    Trying my best okay im trying

---
+++ ArtisCraft/Source/GameStates/BaseState.h
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+class MasterRenderer;
+class Application;
+
+class BaseState {
+public:
+	BaseState(Application &app) : m_pApplication(&app) {};
+
+	virtual ~BaseState() = default;
+	
+	virtual void handleEvent(sf::Event e) = 0;
+	virtual void handleInput() = 0;
+	virtual void update(float deltaTime) = 0;
+	virtual void render(MasterRenderer renderer) = 0;
+
+protected:
+	Application* m_pApplication;
+
+};
