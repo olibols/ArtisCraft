f8aa3fc1485d4600b95ce476ce4c362cb1518cd1
Oliver Donnellan <odonnellan@student.strode-college.ac.uk>
2020-01-20 23:07:44 +0000

    Added the structure builder

---
+++ ArtisCraft/Source/World/Terrain/StructureBuilder.cpp
@@ -0,0 +1,27 @@
+#include "StructureBuilder.h"
+
+void StructureBuilder::build(Chunk* chunk)
+{
+	for (auto& block : m_blocks) {
+		chunk->setBlock(block.x, block.y, block.z, block.block);
+	}
+}
+
+void StructureBuilder::addBlock(int x, int y, int z, BlockID block)
+{
+	m_blocks.emplace_back(block, x, y, z);
+}
+
+void StructureBuilder::addColumn(int startX, int startY, int startZ, int height, BlockID block)
+{
+	for (int y = 0; y < height; y++) {
+		m_blocks.emplace_back(block, startX, startY + y, startZ);
+	}
+}
+
+void StructureBuilder::fill(int startX, int startZ, int y, int endX, int endZ, BlockID block)
+{
+	for (int x = startX; x < endX; x++)
+		for (int z = startZ; z < endZ; z++)
+			addBlock(x, y, z, block);
+}
---
+++ ArtisCraft/Source/World/Terrain/StructureBuilder.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../BlockID.h"
+#include "../Chunk/Chunk.h"
+
+#include <vector>
+
+struct BuilderBlock {
+	BuilderBlock(BlockID block, int x, int y, int z) : block(block), x(x), y(y), z(z) {};
+
+	BlockID block;
+	int x, y, z;
+};
+
+class StructureBuilder {
+public:
+	StructureBuilder() = default;
+
+	void build(Chunk* chunk);
+
+	void addBlock(int x, int y, int z, BlockID block);
+
+	void addColumn(int startX, int startY, int startZ, int height, BlockID block);
+	void fill(int startX, int startZ, int y, int endX, int endZ, BlockID block);
+private:
+	std::vector<BuilderBlock> m_blocks;
+};
